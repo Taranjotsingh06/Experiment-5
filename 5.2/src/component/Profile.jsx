707e05a2f86a7de566e8f49a47003c382a4f7927
Taranjotsingh06 <taranjotsingh12006@gmail.com>
2026-02-15 20:08:26 +0530

    Added Experiment 5 - Lazy Loading in React

---
+++ 5.2/src/component/Profile.jsx
@@ -0,0 +1,20 @@
+function Profile() {
+  return (
+    <>
+      <h1>Profile</h1>
+
+      <img src="/My-photo.jpg" alt="My Photo" width="200" />
+
+      <h3>Name: TARANJOT SINGH</h3>
+      <h3>UID: 23BAI70077</h3>
+
+      <h2>Projects:</h2>
+      <ul>
+        <li>Library Management</li>
+        <li>Voice Recognition</li>
+      </ul>
+    </>
+  );
+}
+
+export default Profile;
